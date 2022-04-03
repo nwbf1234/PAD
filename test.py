8c7165e3167bb2dedc817b0de11e4ca9a8722a07
Sam <nwbf1234.1@gmail.com>
2022-04-03 20:32:14 +0800

    0403

---
+++ test.py
@@ -0,0 +1,17 @@
+
+from argparse import ArgumentParser
+
+import torchvision.transforms as transforms
+import torch
+
+def parse_args():
+    
+    parser = ArgumentParser()
+
+    parser.add_argument("--batch_size", type=int,default=64)
+    parser.add_argument('--sample_size',type=int,default=256)    
+
+def main(args):
+    torch.cuda.set_device(0)
+    device = torch.device('cuda',0)
+    T = transforms.Compose([transforms.Resize((args.sample_size,args.sample_size)),transforms.ToTensor()]) 
